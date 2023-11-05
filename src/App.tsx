3960c46bf70a6ed3f57525519de57d07a5321237
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-06 00:38:19 +0530

    add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import DateCounter from "./DateCounter";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="bg-gray-800 h-screen">
       <Header></Header>
+      <Main>
+        <p>1/15</p>
+        <span>Questions?</span>
+      </Main>
     </div>
   );
 }
