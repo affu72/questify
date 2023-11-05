4841d8fb85f5843eeeae4933775d127eb80851a1
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-05 11:56:48 +0530

    state management using reducer hook

--- src/DateCounter.tsx
+++ src/DateCounter.tsx
@@ -1,24 +1,66 @@
-import { useState } from "react";
+import {useReducer} from "react";
+
+enum ActionsKind {
+  inc = "inc",
+  dec = "dec",
+  setCount = "setCount",
+  setSteps = "setSteps",
+  reset = "reset",
+}
+
+interface Actions {
+  type: ActionsKind;
+  payload?: number;
+}
+
+interface State {
+  count: number;
+  steps: number;
+}
+
+function reducer(state: State, action: Actions): State {
+  const {count, steps} = state;
+  if (action.type === "inc") return {...state, count: count + steps};
+  if (action.type === "dec") return {...state, count: count - steps};
+  if (action.type === "setCount") return {...state, count: action.payload ?? 0};
+  if (action.type === "setSteps") return {...state, steps: action.payload ?? 0};
+  if (action.type === "reset") return {count: 0, steps: 1};
+
+  throw new Error("Unknown Action");
+}
 
 export default function DateCounter() {
-  const [steps, setSteps] = useState<number>(1);
-  const [value, setValue] = useState<number>(0);
+  // const [steps, setSteps] = useState<number>(1);
+  // const [value, setValue] = useState<number>(0);
+
+  const initialvalue = {count: 0, steps: 1};
+
+  const [state, dispatch] = useReducer(reducer, initialvalue);
+
+  const {count, steps} = state;
 
   function handleInc() {
-    setValue(v => v+steps);
+    // setValue(v => v + steps);
+    // dispatch({type: "inc", payload: 1});
+
+    dispatch({type: ActionsKind.inc});
   }
 
   function handleDec() {
-    setValue((v)=>v-steps)
+    // setValue(v => v - steps);
+    // dispatch({type: "dec", payload: 1});
+
+    dispatch({type: ActionsKind.dec});
   }
 
   function reset() {
-    setSteps(1);
-    setValue(0);
+    // setSteps(1);
+    // setValue(0);
+    dispatch({type: ActionsKind.reset});
   }
 
   const date = new Date();
-  date.setDate(date.getDate() + value);
+  date.setDate(date.getDate() + count);
 
   return (
     <div className="flex w-full h-screen bg-slate-700 items-center justify-center flex-col text-white gap-4">
@@ -28,7 +70,9 @@ export default function DateCounter() {
           min={1}
           max={10}
           className="outline-none"
-          onChange={e => setSteps(+e.target.value)}
+          onChange={e =>
+            dispatch({type: ActionsKind.setSteps, payload: +e.target.value})
+          }
           value={steps}
         />
         <span className="font-semibold">{steps}</span>
@@ -40,8 +84,10 @@ export default function DateCounter() {
         <input
           type="number"
           className="py-1 px-2 outline-none text-black"
-          onChange={e => setValue(+e.target.value)}
-          value={value ?? ""}
+          onChange={e =>
+            dispatch({type: ActionsKind.setCount, payload: +e.target.value})
+          }
+          value={state.count ?? ""}
         />
         <button className="p-2 bg-gray-800 aspect-square" onClick={handleInc}>
           &#43;
@@ -49,7 +95,9 @@ export default function DateCounter() {
       </div>
       <p>{date.toDateString()}</p>
 
-      <button className="px-4 py-2 bg-gray-800" onClick={reset}>Reset</button>
+      <button className="px-4 py-2 bg-gray-800" onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 }
