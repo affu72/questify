07ede15f24a0010cc8d8160723cc6f722f7d054a
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-10 01:16:13 +0530

    fix main style

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import {ReactNode} from "react";
+
 export default function Main({children}: {children: ReactNode}) {
-  return <main className="text-white">{children}</main>;
+  return <main className="text-white w-3/4 max-w-2xl">{children}</main>;
 }
