80b7002e87facb57b6f40230b6be2463a329a73b
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-10 01:15:42 +0530

    fix header

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,9 +2,9 @@ import logo from "./assets/logo.svg";
 
 export default function Header() {
   return (
-    <header className="w-full h-48 text-white flex items-center gap-8 pt-16 p-10 justify-center">
-      <img src={logo} alt="questify-logo" className="w-40" />
-      <h1 className="text-3xl sm:text-5xl md:text-7xl text-slate-200 uppercase font-dots flex grow-1 flex-col justify-center items-center gap-2 tracking-wide">
+    <header className="grid grid-cols-[8rem_auto] items-center justify-center gap-x-6">
+      <img src={logo} alt="questify-logo" className="w-full" />
+      <h1 className="text-2xl sm:text-4xl md:text-5xl text-slate-200 uppercase font-dots flex grow-1 flex-col justify-center items-center gap-2 tracking-wide">
         <span>Questify</span>
         <span>The Quiz App</span>
       </h1>
