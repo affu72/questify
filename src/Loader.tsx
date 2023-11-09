80b196d9be8a3a82e99a928be4a2f0845309c49a
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-10 01:15:55 +0530

    loader component

---
+++ src/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="w-10 animate-loader aspect-square rounded-full border-y-slate-200 border-x-gray-700 border-8"></div>
+    </div>
+  );
+}
